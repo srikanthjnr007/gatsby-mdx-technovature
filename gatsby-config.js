75e2a6100c2c48feb93b50c6ffc1a4da6ad62614
Srikanth J <jnsrikanth@yahoo.com>
2020-03-05 14:20:23 +0530

    config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
 
-    title: `Technovature Software`,
-    author: `Srikanth Jallapuram`,
+    title: `Apple Software`,
+    author: `Steve Jobs`,
     description: `High Technology Digital and Software Development company in Mobile, cloud, Big Data, Internet of Things, IoT, Data Sciences, Artificial Intelligence and Software Automation`,
-    siteUrl: `http://www.technovature.com`,
+    siteUrl: `http://www.apple-dui.com`,
     social: {
-      twitter: `technovature`,
+      twitter: `apple-2`,
       fbAppId: ' ',
     },
   },
