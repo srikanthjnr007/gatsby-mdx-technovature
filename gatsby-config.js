8497305740240723f8cb956c48a683e0dea57779
jnsrikanth <srikanthjnr@yahoo.com>
2020-07-18 21:23:48 +0530

    Added gatsby-plugin-preload-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-plugin-sass',
+    `gatsby-plugin-preload-fonts`,
      { 
       resolve: `gatsby-plugin-purgecss`,
       options: {
